2e5eef8d2d9710c7613dadb4c386499ca2f46ee8
bagustris <bagustris@linuxmail.org>
2015-04-22 08:10:46 +0700

    modified:   bintangJumlah.cpp 	modified:   deretGeo.cpp 	modified:   deretGeo2.cpp 	new file:   primaFungsi.cpp

--- bintangJumlah.cpp
+++ bintangJumlah.cpp
@@ -1,13 +1,19 @@
-#include <iostream.h>
+#include <iostream>
+
+using namespace std;
+
+main()
 {
 int i,j,n,a;
 cout<<"masukkan n="; cin>>n;
 for (i=1; i<=n; i++) 
 { 
-for (j=1;j<=i;j++)
-cout <<"*";
-a=i;
-a=a+1; 
-cout<<endl; }
+    for (j=1;j<=i;j++)
+    {
+    cout <<"*";
+    a=a+1;
+    } 
+cout<<endl; 
+}
 cout<<"Jumlah bintang="<<a<<endl;
 }
--- deretGeo.cpp
+++ deretGeo.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
+//#include <math.h>
 
 using namespace std;
 
 int main()
 {
-int i, n, a=1;
-cout<<"Masukkan N: ";
-cin>>n;
-for(i=1; i<=n; i++)
+    int i,a,r,n,jumlah=0;
+    cout<<"Program Deret Bilangan \n";
+    cout<<"======================\n";
+    cout<<"Masukkan Bilangan Pertama: ";
+    cin>>a;
+    cout<<"Masukkan Banyak Bilangan: ";
+    cin>>n;
+    cout<<"======================\n";
+    cout<<"Deret Bilangan adalah: ";
+
+    //jumlah=0;
+
+    for(i=1; i<=n; i++)
     {
-    cout<<a<<endl;
-    a=a+4;
+        // r=pow((i+1),2);
+        r=(i+1)*(i+1);
+        cout<<a<<" ";
+        jumlah=jumlah+a;
+        a=a+r;
     }
+    cout<<"\n";
+    cout<<"Jumlah bilangan:" << jumlah <<"\n";
+    return 0;
 }
