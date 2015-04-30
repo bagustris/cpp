efc2f34f7e4ae24c03979cb35433f761a6ad4580
bagustris <bagustris@yahoo.com>
2015-05-01 06:47:42 +0700

    new file:   passBy0.cpp 	new file:   passBy1.cpp

---
+++ passBy0.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void by_val(int arg) { 
+    arg += 2; 
+}
+void by_ref(int&arg) { 
+    arg += 2; 
+}
+
+int main()
+{
+    int x = 0;
+    by_val(x); 
+    cout << x << endl;  // prints 0
+    by_ref(x); 
+    cout << x << endl;  // prints 2
+
+    int y = 0;
+    by_ref(y); 
+    cout << y << endl;  // prints 2
+    by_val(y); 
+    cout << y << endl;  // prints 2
+}
---
+++ passBy1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void Modify(int p, int * q, int * o)
+{
+    p = 27; // passed by value - only the local parameter is modified
+    *q = 27; // passed by value or reference, check call site to determine which
+    *o = 27; // passed by value or reference, check call site to determine which
+}
+
+int main()
+{
+    int a = 1;
+    int b = 1;
+    int x = 1;
+    int * c = &x;
+    cout<<a<<' '<<b<<' '<<c<<' '<<x<<'\n';
+    Modify(a, &b, c);   // a is passed by value, b is passed by reference by creating a pointer,
+                        // c is a pointer passed by value
+    // b and x are changed
+    cout<<a<<' '<<b<<' '<<c<<' '<<x<<'\n';
+    return(0);
+}
+
